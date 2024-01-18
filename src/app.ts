2743333dc68040c4eaa9e6905cdbd1dccbee9ac5
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2024-01-18 15:16:20 -0300

    configuring Docker

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,16 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
 
 // ORM - Object Realational Mapper
+// Instancio para fazer a conexão com DB
+const prisma = new PrismaClient()
+
+// Crio um user
+prisma.user.create({
+  data: {
+    name: 'Gabriel Lacerda',
+    email: 'gablacerd@gmail.com',
+  },
+})
