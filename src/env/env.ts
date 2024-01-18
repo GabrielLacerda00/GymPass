f7882e1b6e04a7acdd41dae4d4cb9a0bdf3758b9
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2024-01-18 12:27:36 -0300

    final config project

---
+++ src/env/env.ts
@@ -0,0 +1,17 @@
+import 'dotenv'
+import { z } from 'zod'
+
+// Construo meu schema das variaveis de ambiente
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+})
+// Valido meu schema
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('❌ Invalid environment variables')
+  throw new Error('Invalid environment variables')
+}
+
+export const env = _env.data
