3166e9ee81807ef48626057906231296c5178bff
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2024-01-22 16:15:36 -0300

    Errors: Creating global handler erros

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -25,9 +25,9 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     })
   } catch (error) {
     if (error instanceof UserAlreadyExistsError) {
-      reply.status(409).send(error.message)
+      reply.status(409).send({ message: error.message })
     }
-    return reply.status(500).send()
+    throw error
   }
 
   return reply.status(201).send()
