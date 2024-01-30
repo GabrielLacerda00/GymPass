26e624fd1855440c3f0175d574abb5a492bd5bf0
Gabriel Lacerda <gabriellacerda@MacBook-Air-de-Gabriel.local>
2024-01-30 15:03:49 -0300

    Creating tests e2e for register controller

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,20 @@
-import { test } from 'vitest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
 
-test('Test01', () => {
-  console.log('')
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+  it('Should be able to register a user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'Gabriel',
+      email: 'gabrol@gmail.com',
+      password: '123456789',
+    })
+    expect(response.statusCode).toEqual(201)
+  })
 })
